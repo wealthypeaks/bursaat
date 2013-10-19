166f1e915f39d8a493a419f5cb4bf2bd6a73ba76
dahan <dahan@inbox.com>
2013-10-19 16:27:28 +0300

    Model creation
    20131019

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-bursaat
-=======
+##Welcome to Wakanda##
 
-Bursaat Project
+This is the place to describe your Wakanda application.
+
+Please visit [www.wakanda.org](http://www.wakanda.org "wakanda.org") for more information.
---
+++ project/Model.js
@@ -0,0 +1,17 @@
+﻿
+guidedModel =// @startlock
+{
+	CommonTranslation :
+	{
+		lang :
+		{
+			events :
+			{
+				onValidate:function(attributeName)
+				{// @endlock
+					// Add your code here
+				}// @startlock
+			}
+		}
+	}
+};// @endlock
